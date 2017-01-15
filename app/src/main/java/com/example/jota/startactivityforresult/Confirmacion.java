a76de51e3b70ae3f8b54e005bd4f1e729daed9eb
Jorge <joterodelrio@danielcastelao.org>
2017-01-15 19:02:37 +0100

    creando fragment

---
+++ app/src/main/java/com/example/jota/startactivityforresult/Confirmacion.java
@@ -0,0 +1,30 @@
+package com.example.jota.startactivityforresult;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Confirmacion extends Fragment {
+
+
+    public Confirmacion() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_confirmacion, container, false);
+        layout.findViewById()
+        return layout;
+    }
+
+}
--- app/src/main/java/com/example/jota/startactivityforresult/MainActivity.java
+++ app/src/main/java/com/example/jota/startactivityforresult/MainActivity.java
@@ -4,20 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        nombre = (EditText)findViewById(R.id.nombre);
     }
 
 
     public void abrirActivity(View v){
-        Intent i = new Intent();
-
-
+        Intent i = new Intent(this,Main2Activity.class);
+        i.putExtra("nombre",nombre.getText());
+        startActivityForResult(i,1);
     }
 }
